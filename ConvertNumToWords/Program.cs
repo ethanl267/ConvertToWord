240dd78b1fc9fdb4a64d07df49fcd60cda2ddc90
ethanl267 <71071509+ethanl267@users.noreply.github.com>
2025-04-23 15:23:45 +0200

    Add files via upload

---
+++ ConvertNumToWords/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace ConvertNumToWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number to convert to words:");
+            string input = Console.ReadLine();
+            int inputnumber = int.Parse(input);
+            Console.WriteLine(inputnumber);
+            
+            /*if (int.TryParse(input, out int number))
+            {
+                string result = ConvertNumber(number);
+                Console.WriteLine($"The number {number} in words is: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+            */
+
+        }
+    }
+}
